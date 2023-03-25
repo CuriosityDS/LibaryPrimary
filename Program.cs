4531c7b5737367a6a0f1d6bbfc44f30523cb5db3
CuriosityDS <CuriosityDS@yandex.ru>
2023-03-25 13:07:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PointLibary;
+namespace LibaryPrimary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(6, 6);
+            Point point2 = new Point(12,6);
+            Point point3 = new Point(12, 1);
+            Point point4 = new Point(1, 12);
+            Rectangle point = new Rectangle(point1, point2, point3, point4);
+            Console.WriteLine(point);
+            Parallelogramm parallelogramm = new Parallelogramm(point1, point2, point3, point4);
+            Console.WriteLine(parallelogramm);
+            Rhomb rhomb = new Rhomb(point1, point2, point3, point4);
+            Console.WriteLine(rhomb);
+            Squar squar = new Squar(point1, point2, point3, point4);
+            Console.WriteLine(squar);
+            Console.ReadKey();
+        }
+    }
+}
